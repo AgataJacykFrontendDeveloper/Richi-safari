213d586d8f52b651a5de829493c134b16a88d761
Agata Jacyk <158597636+AgataJacykFrontendDeveloper@users.noreply.github.com>
2024-09-18 15:24:27 +0200

    Header links fixed

--- src/layout/MainHeader.js
+++ src/layout/MainHeader.js
@@ -22,27 +22,27 @@ const MainHeader = () => {
       </div>
   </div>
 </nav>
-<nav class="navbar fixed-top sm-nav">
+<nav class="navbar sm-nav">
   <div class="container-fluid">
     <button class="navbar-toggler btn41-43 btn-42 btn-sand" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" data-bs-theme="dark">
       <span class="navbar-toggler-icon" data-bs-theme="dark"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-      <Link className="nav-link active" to="/"> <img className="header-logo" src={require("../assets/darkLogo.png")}
-        /></Link>
+      <a className="nav-link active" href="https://richi-safari.vercel.app"> <img className="header-logo" src={require("../assets/darkLogo.png")}
+        /></a>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-<Link className="nav-link active" to="/">Strona Główna</Link>
+<a className="nav-link active" href="https://richi-safari.vercel.app">Strona Główna</a>
           </li>
           <li class="nav-item">
-          <Link className="nav-link" to="/">O Safari</Link>
+          <a className="nav-link" href="https://richi-safari.vercel.app/przed-wyjazdem">Przed wyjazdem</a>
           </li>
           <li class="nav-item">
-          <Link className="nav-link" to="/oferta">Oferta</Link>
+          <a className="nav-link" href="https://richi-safari.vercel.app/oferta">Oferta</a>
           </li>
         </ul>
       </div>
